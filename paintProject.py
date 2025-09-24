362284af745ccb1b6d0c3b3d0cab5fa33846101a
nkdvv <78442659+nkdvv@users.noreply.github.com>
2025-09-23 18:11:03 -0700

    Add paint project script for calculating costs

---
+++ paintProject.py
@@ -0,0 +1,41 @@
+# Nikki Davis & Ridge Torregosa
+# Sept. 23rd, 2025
+# Paint Project
+
+# STEP ONE
+# Request usser input
+width = float(input("Wall width: "))
+height = float(input("Wall height: "))
+cost = float(input("Cost of one paint can: "))
+
+# Output
+area = float(width * height)
+print(f'Wall area: {area: .1f} sq ft') # needs format specifier
+
+# STEP TWO 
+# Calculate the amount of paint by the third decimal
+# * one gallon of paint covers 350 sq ft *
+coverage = float(area/350)
+
+# Output
+print(f'Paint needed: {coverage: .2f} gallons.')
+
+# STEP THREE
+# Calculate the number of gallon cans needed to paint the wall area
+
+totalCans = math.ceil(coverage)
+
+# Output
+print("Cans neede: ", totalCans, "cans(s)")
+
+# STEP FOUR
+# Calculate and output the paint cost, sales tax of 7%, and total cost. 
+SALES_TAX = 0.07
+cost2 = float(cost * totalCans)
+tax = float(cost2 * SALES_TAX)
+totalCost = float(cost2 + tax)
+
+# Output
+print(f'Paint cost: ${cost: .2f}')
+print(f'Sales tax: ${tax: .2f}')
+print(f'Total cost: ${totalCost: .2f}')
